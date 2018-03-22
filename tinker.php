fb63df5dbde066f9318d597b1a863180cfe83fae
Jeremy Ward <jeremy.ward@webdevstudios.com>
2018-03-21 19:48:06 -0500

    Create bootstrap file to set up the application.

---
+++ tinker.php
@@ -0,0 +1,18 @@
+#!/usr/bin/env php
+<?php
+/**
+ * Tinker: A Symfony console application that doesn't really do anything. Yet?
+ *
+ * @author Jeremy Ward <jeremy@jmichaelward.com
+ * @package JMW\Tinker
+ */
+
+namespace JMW\Tinker;
+
+require __DIR__ . '/vendor/autoload.php';
+
+use Symfony\Component\Console\Application;
+
+$app = new Application();
+
+$app->run();
