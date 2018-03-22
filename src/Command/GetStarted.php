9bb1303a38896c99c542d9699d5c092999db952d
Jeremy Ward <jeremy.ward@webdevstudios.com>
2018-03-21 20:33:39 -0500

    Set up a config class that we probably won't need, just in case. Scaffold our first command.

---
+++ src/Command/GetStarted.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * @package JMW\Tinker\Command
+ */
+
+namespace JMW\Tinker\Command;
+
+use JMW\Tinker\Config;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Class GetStarted
+ */
+class GetStarted extends Command
+{
+    /**
+     * Configure the command.
+     *
+     * @see https://symfony.com/doc/current/console.html#configuring-the-command
+     */
+    protected function configure()
+    {
+        $this->setName(Config::APP_NAME.':start')
+            ->setDescription('The command that started it all!')
+            ->setHelp("This command doesn't really do anything meaningful, but then, neither does Tinker.");
+    }
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @return int|null|void
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        parent::execute($input, $output);
+    }
+}
---
+++ src/Config.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * A config class because maybe we'll need this too. Ain't no harm in tryin'.
+ *
+ * It's highly likely I want to pass this into every object that needs it, but I'm just going to go with some
+ * constant values for now to make things easier.
+ *
+ * @package JMW\Tinker
+ */
+
+namespace JMW\Tinker;
+
+class Config
+{
+    const APP_NAME = 'tinker';
+}
